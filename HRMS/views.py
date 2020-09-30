b05dc4d076f6f086652db46e7d1ec66f4a444831
krsatz <sathish@linkeddots.com>
2020-09-30 23:45:27 +0530

    included html

--- HRMS/views.py
+++ HRMS/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
+from django.template import loader
 from datetime import datetime
 
 # Create your views here.
 from django.http import HttpResponse
 
 def homePage(reqest):
+    template = loader.get_template('HRMS/login.html')
     
-    return HttpResponse("Hello Developer!")
+    context = {"name":"D-Shark"}
+    #return render(reqest,'HRMS/login.html')
+    return HttpResponse(template.render(context,reqest))
     
     
